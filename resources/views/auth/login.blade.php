e1cb24c4a1737efc32637e3a2f7c85fb32512840
Alendroide <alendroideyt@gmail.com>
2026-02-01 22:20:42 -0500

    Auth pages style added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,13 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login | AutoHub</title>
+    <link rel="stylesheet" href="/css/auth.css">
+    <link rel="stylesheet" href="/css/global.css">
 </head>
 <body>
     <form method="POST" action="{{route('login.post')}}">
         @csrf
-        <input type="email" name="email" value="{{old('email')}}" required placeholder="pepe@example.com">
-        <input type="password" name="password" required placeholder="********">
-        <button type="submit">Submit</button>
+        <x-atoms.custom-input type="email" name="email" value="{{old('email')}}" required placeholder="pepe@example.com" />
+        <x-atoms.custom-input type="password" name="password" required placeholder="********" />
+        <a href="{{route("register")}}">No tienes una cuenta? Regístrate aquí</a>
+        <div>
+            <x-atoms.custom-button type="submit" color="primary-button">Iniciar sesión</x-atoms.custom-button>
+        </div>
     </form>
     @error('email')
         <p>Error iniciando sesión</p>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,15 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Register | AutoHub</title>
+    <link rel="stylesheet" href="/css/auth.css">
+    <link rel="stylesheet" href="/css/global.css">
 </head>
 <body>
     <form method="POST" action="{{route('register.post')}}">
         @csrf
-        <input type="text" name="name" required placeholder="Pepe Bonilla">
-        <input type="email" name="email" required placeholder="pepe@example.com">
-        <input type="password" name="password" required placeholder="********">
-        <input type="password" name="password_confirmation" required placeholder="********">
-        <button type="submit">Submit</button>
+        <x-atoms.custom-input type="text" name="name" required placeholder="Pepe Bonilla" />
+        <x-atoms.custom-input type="email" name="email" required placeholder="pepe@example.com" />
+        <x-atoms.custom-input type="password" name="password" required placeholder="********" />
+        <x-atoms.custom-input type="password" name="password_confirmation" required placeholder="********" />
+        <a href="{{route("login")}}">Ya tienes una cuenta? Inicia sesión aquí</a>
+        <div>
+            <x-atoms.custom-button type="submit" color="primary-button">Registrarte</x-atoms.custom-button>
+        </div>
     </form>
 </body>
 </html>
