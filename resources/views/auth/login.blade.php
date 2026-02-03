2b01f6f37b2b541259eae6d669fd86cb1880008b
Alendroide <alendroideyt@gmail.com>
2026-02-02 23:33:41 -0500

    Validation of auth forms

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,18 @@
 @extends('layouts.auth')
 @section('title') Login @endsection
 @section('content')
-    <form method="POST" action="{{route('login.post')}}">
+    <form id="login-form" method="POST" action="{{route('login.post')}}">
         <img src="{{ asset("/img/logo-dark.png") }}" alt="logo">
         @csrf
-        <x-atoms.custom-input type="email" name="email" value="{{old('email')}}" required placeholder="pepe@example.com" />
-        <x-atoms.custom-input type="password" name="password" required placeholder="********" />
+        <x-atoms.custom-input type="email" maxLength="40" name="email" value="{{old('email')}}" required placeholder="pepe@example.com" />
+        <x-atoms.custom-input type="password" minLength="8" name="password" required placeholder="********" />
         <a href="{{route("register")}}">No tienes una cuenta? Regístrate aquí</a>
         <div>
             <x-atoms.custom-button type="submit" color="primary-button">Iniciar sesión</x-atoms.custom-button>
         </div>
     </form>
     @error('email')
-        <p>Error iniciando sesión</p>
+        <p class="error">Credenciales incorrectas</p>
     @enderror
+    <script>$("#login-form").validate();</script>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,16 +1,17 @@
 @extends('layouts.auth')
 @section('title') Register @endsection
 @section('content')
-    <form method="POST" action="{{route('register.post')}}">
+    <form id="register-form" method="POST" action="{{route('register.post')}}">
         <img src="{{ asset("/img/logo-dark.png") }}" alt="logo">
         @csrf
-        <x-atoms.custom-input type="text" name="name" required placeholder="Pepe Bonilla" />
-        <x-atoms.custom-input type="email" name="email" required placeholder="pepe@example.com" />
-        <x-atoms.custom-input type="password" name="password" required placeholder="********" />
-        <x-atoms.custom-input type="password" name="password_confirmation" required placeholder="********" />
+        <x-atoms.custom-input type="text" maxLength="40" name="name" required placeholder="Pepe Bonilla" />
+        <x-atoms.custom-input type="email" maxLength="40" name="email" required placeholder="pepe@example.com" />
+        <x-atoms.custom-input type="password" minLength="8" name="password" required placeholder="********" />
+        <x-atoms.custom-input type="password" minLength="8" name="password_confirmation" required placeholder="********" />
         <a href="{{route("login")}}">Ya tienes una cuenta? Inicia sesión aquí</a>
         <div>
             <x-atoms.custom-button type="submit" color="primary-button">Registrarte</x-atoms.custom-button>
         </div>
     </form>
+    <script>$("#register-form").validate();</script>
 @endsection
