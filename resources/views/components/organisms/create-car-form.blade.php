a6776b23cf30c1c9645194ed9323333548b27702
Alendroide <alendroideyt@gmail.com>
2026-02-03 09:03:24 -0500

    Car line minLength removed

--- resources/views/components/organisms/create-car-form.blade.php
+++ resources/views/components/organisms/create-car-form.blade.php
@@ -2,7 +2,7 @@
     <form id="create-car-form" style="display: flex; flex-direction: column; gap: 1rem;">
         @csrf
         <x-atoms.custom-input type="text" minLength="3" maxLength="255" name="brand" placeholder="Marca" required />
-        <x-atoms.custom-input type="text" minLength="3" maxLength="255" name="line" placeholder="Linea" required />
+        <x-atoms.custom-input type="text" maxLength="255" name="line" placeholder="Linea" required />
         <x-atoms.custom-input type="number" min="1980" max="2026" name="model" placeholder="Modelo" required />
         <x-atoms.custom-input type="text" minLength="6" maxLength="8" name="plate" placeholder="Placa" required />
         <x-atoms.custom-input type="text" minLength="3" maxLength="255" name="color" placeholder="Color" required />
--- resources/views/components/organisms/update-car-form.blade.php
+++ resources/views/components/organisms/update-car-form.blade.php
@@ -5,7 +5,7 @@
         <input type="hidden" name="id" />
 
         <x-atoms.custom-input type="text" minLength="3" maxLength="255" name="brand"  placeholder="Marca" required />
-        <x-atoms.custom-input type="text" minLength="3" maxLength="255" name="line"   placeholder="Linea" required />
+        <x-atoms.custom-input type="text" maxLength="255" name="line"   placeholder="Linea" required />
         <x-atoms.custom-input type="number" min="1980" max="2026" name="model"  placeholder="Modelo" required />
         <x-atoms.custom-input type="text" minLength="6" maxLength="8" name="plate" placeholder="Placa" required />
         <x-atoms.custom-input type="text" minLength="3" maxLength="255" name="color"  placeholder="Color" required />
