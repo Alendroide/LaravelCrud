a168a132fe88ff5eae5777fdcc3aad19c6004992
Alendroide <alendroideyt@gmail.com>
2026-02-03 14:57:31 -0500

    CSS improved to keep mobile sidebar closed and desktop sidebar opened

--- public/js/global.js
+++ public/js/global.js
@@ -1,23 +1,11 @@
 $(document).ready(function() {
-    const mq = window.matchMedia('(max-width: 767px)');
-    
-    function syncSidebarState(e) {
-        if (e.matches) {
-            $('body').addClass('sidebar-closed');
-        } else {
-            $('body').removeClass('sidebar-closed');
-        }
-    }
-    
-    syncSidebarState(mq);
-    mq.addEventListener('change', syncSidebarState);
     
     $("#sidebar-overlay").on("click", function() {
-        $("body").toggleClass("sidebar-closed");
+        $("body").toggleClass("sidebar-state");
     })
 
     $("#sidebar-toggle").on("click", function(){
-        $("body").toggleClass("sidebar-closed");
+        $("body").toggleClass("sidebar-state");
     })
 })
 
