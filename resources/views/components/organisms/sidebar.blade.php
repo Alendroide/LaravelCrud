d893666904a98416f1ff2c64d030c8d68ed947ef
Alendroide <alendroideyt@gmail.com>
2026-02-04 16:41:36 -0500

    Sidebar selected active added

--- resources/views/components/organisms/sidebar.blade.php
+++ resources/views/components/organisms/sidebar.blade.php
@@ -2,7 +2,7 @@
     <a style="margin: 2rem 0;" href="{{ route('dashboard') }}">
         <img src="{{ asset('img/logo.png') }}" id="sidebar-logo" alt="sidebar-logo">
     </a>
-    <a href="{{ route("dashboard") }}" class="sidebar-item">Inicio</a>
-    <a href="{{ route("my-vehicles") }}" class="sidebar-item">Mis vehículos</a>
-    <a href="{{ route("my-archives") }}" class="sidebar-item">Archivo</a>
+    <a href="{{ route("dashboard") }}" class="sidebar-item {{ request()->routeIs('dashboard') ? 'active' : '' }}">Inicio</a>
+    <a href="{{ route("my-vehicles") }}" class="sidebar-item {{ request()->routeIs('my-vehicles') ? 'active' : '' }}">Mis vehículos</a>
+    <a href="{{ route("my-archives") }}" class="sidebar-item {{ request()->routeIs('my-archives') ? 'active' : '' }}">Archivo</a>
 </div>
