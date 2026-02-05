c58a139461846c58aa0aeafc1493523e75d6b06c
Alendroide <alendroideyt@gmail.com>
2026-02-04 21:04:12 -0500

    Deleted unnecesary function

--- public/js/global.js
+++ public/js/global.js
@@ -8,23 +8,3 @@ $(document).ready(function() {
         $("body").toggleClass("sidebar-state");
     })
 })
-
-// Custom rule to check for max size in file inputs
-
-$.validator.addMethod(
-    "filesize",
-    function (value, element, maxSize) {
-        if (this.optional(element)) return true;
-
-        if (!element.files || !element.files.length) return true;
-
-        for (let i = 0; i < element.files.length; i++) {
-            if (element.files[i].size > maxSize) {
-                return false;
-            }
-        }
-
-        return true;
-    },
-    "Uno o más archivos superan el tamaño permitido"
-);
