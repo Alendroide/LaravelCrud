617e6cd363e14de2c0ac7738ee0d5ec139170379
Alendroide <alendroideyt@gmail.com>
2026-02-04 19:29:12 -0500

    Edit button hidden on dashboard page

--- public/js/cars/car-actions.js
+++ public/js/cars/car-actions.js
@@ -1,6 +1,6 @@
 let cars = [];
 
-function renderCars() {
+function renderCars(showEditButton = false) {
     if (!cars || cars.length === 0) {
         $("#cars").html("<p>No se encontraron vehículos.</p>");
         return;
@@ -8,7 +8,7 @@ function renderCars() {
 
     $("#cars").html(cars.map(car => `
         <div class="car-card" data-id="${car.id}">
-            ${car.owner_id === window.AUTH_USER_ID ?
+            ${(car.owner_id === window.AUTH_USER_ID) && showEditButton ?
                 `<div class="edit-button" onclick="showUpdateModal(${car.id})">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pencil-icon lucide-pencil"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/><path d="m15 5 4 4"/></svg>
                 </div>` : ""
--- public/js/cars/fetching/my-archives.js
+++ public/js/cars/fetching/my-archives.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         dataType: "json",
         success: function(response, status) {
             cars = response.data;
-            renderCars();
+            renderCars(true);
             renderPagination(response.links);
         },
         error: function(xhr, status, error) {
