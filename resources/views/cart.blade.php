041502db4cedc49a3070bcaf2dcfea10e07261ac
Alendroide <alendroideyt@gmail.com>
2026-02-06 15:18:45 -0500

    small tweak in cart blade

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -6,6 +6,7 @@
 
     <div id="cart-total" class="mt-4"></div>
 
+    <link rel="stylesheet" href="{{ asset("css/cars/global.css") }}">
     <link rel="stylesheet" href="{{ asset("css/cars/cart.css") }}">
     <script src="{{ asset("js/cars/cart.js") }}"></script>
 @endsection
