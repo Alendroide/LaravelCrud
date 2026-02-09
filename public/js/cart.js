ab7da7d3e298454ad3934814a5bd596c4989ea00
Alendroide <alendroideyt@gmail.com>
2026-02-09 09:01:20 -0500

    Spinner added to cart

--- public/js/cart.js
+++ public/js/cart.js
@@ -89,7 +89,7 @@ function renderCartList() {
                     >
                 </div>
             </div>
-            <button class="btn btn-outline-success d-flex align-items-center" onclick="checkoutCart()">
+            <button id="buy-cart-button" class="btn btn-outline-success d-flex align-items-center" onclick="checkoutCart()">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-dollar-sign-icon lucide-dollar-sign"><line x1="12" x2="12" y1="2" y2="22"/><path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"/></svg>
                 Comprar
             </button>
@@ -136,6 +136,13 @@ function checkoutCart() {
         showToast("Debe ingresar cédula y nombre del cliente", "error");
         return;
     }
+    
+    const originalBtnHtml = $("#buy-cart-button").html();
+    $("#buy-cart-button").prop("disabled", true).html(`
+        <div style="width: 1.5rem;">
+            <svg fill="#198754" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" opacity=".25"/><path d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z"><animateTransform attributeName="transform" type="rotate" dur="0.75s" values="0 12 12;360 12 12" repeatCount="indefinite"/></path></svg>
+        </div>`
+    ).addClass("disabled-button");
 
     $.ajax({
         url: "/purchase",
@@ -161,6 +168,9 @@ function checkoutCart() {
                 message = xhr.responseJSON.message;
             }
             showToast(message, "error");
+        },
+        complete() {
+            $("#buy-cart-button").prop("disabled", false).html(originalBtnHtml).removeClass("disabled-button");
         }
     });
 
